631ab8f98f5d50e36f8d47d6d996a6d1c288af84
shagaranasution <shagaranst@gmail.com>
2024-12-18 11:17:32 +0700

    move authOptions to libs

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
