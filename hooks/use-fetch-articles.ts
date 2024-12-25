9ecc6c1043214be1bf5f0beb17932c9fe0545b74
Shagara Nasution <shagaranst@gmail.com>
2024-12-25 11:43:39 +0700

    add prologue content
    * add prologue constant
    
    * remove unused file

--- hooks/use-fetch-articles.ts
+++ hooks/use-fetch-articles.ts
@@ -1,5 +1,6 @@
 import type { ArticleWithRelations } from '@/lib/data';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import PROLOGUE_ARTICLE from '@/prologue-article';
 
 export default function useFetchArticles({
   initialPage = 1,
@@ -14,40 +15,45 @@ export default function useFetchArticles({
   const [page, setPage] = useState(initialPage);
   const [hasMore, setHasMore] = useState(true);
 
+  const fetchData = useCallback(
+    async (pageToFetch: number) => {
+      const path = `/api/articles?page=${pageToFetch}&limit=${limit}`;
+      setLoading(true);
+
+      try {
+        const res = await fetch(path);
+
+        if (!res.ok) {
+          setError(res.statusText);
+          return;
+        }
+
+        const { data: newData }: { data: ArticleWithRelations[] } =
+          await res.json();
+
+        setData((prevArticles) => {
+          const existingIds = new Set(
+            prevArticles.map((article) => article.id)
+          );
+          const uniqueNewData = [PROLOGUE_ARTICLE, ...newData].filter(
+            (article) => !existingIds.has(article.id)
+          );
+
+          return [...prevArticles, ...uniqueNewData];
+        });
+        setHasMore(newData.length === limit);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [limit]
+  );
+
   useEffect(() => {
     fetchData(page);
-  }, [page]);
-
-  const fetchData = async (pageToFetch: number) => {
-    const path = `/api/articles?page=${pageToFetch}&limit=${limit}`;
-    setLoading(true);
-
-    try {
-      const res = await fetch(path);
-
-      if (!res.ok) {
-        setError(res.statusText);
-        return;
-      }
-
-      const { data: newData }: { data: ArticleWithRelations[] } =
-        await res.json();
-
-      setData((prevArticles) => {
-        const existingIds = new Set(prevArticles.map((article) => article.id));
-        const uniqueNewData = newData.filter(
-          (article) => !existingIds.has(article.id)
-        );
-
-        return [...prevArticles, ...uniqueNewData];
-      });
-      setHasMore(newData.length === limit);
-    } catch (error: any) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+  }, [fetchData, page]);
 
   const loadMore = () => {
     if (hasMore) setPage((prevPage) => prevPage + 1);
